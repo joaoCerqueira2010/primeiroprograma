8678874de10e4f7614ea2268baae3b9ed3af3fa7
joaoCerqueira2010 <joao.cerqueira.nascimento@escola.pr.gov.br>
2025-02-20 12:11:36 -0300

    Add files via upload

---
+++ primeiro.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int paulo;
+    int jose;
+    int soma;
+    int valormult;           //multiplicacao
+    //soma de paulo e jose
+    paulo=-54;
+    jose=78;
+    soma=paulo+jose;
+    valormult=paulo*jose;
+    printf("o valor de paulo e igual: %d \n",paulo);
+    printf("o valor de jose e igual:%d \n",jose);
+    printf("soma e igual : %d \n",soma);
+    printf("valor da multiplicacao e igual a :  %d \n",valormult);
+    
+    
+
+    return 0;
+}
